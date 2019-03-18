73087684337415c6560dd847098b098c2b42696b
BoredLama <43473329+BoredLama@users.noreply.github.com>
2019-03-18 18:17:03 +0200

    Config file

---
+++ src/config.js
@@ -0,0 +1,25 @@
+
+const DEFAULT_ID = 'stremio_porn'
+
+class Config {
+
+	static DEFAULT_ID = DEFAULT_ID
+	static STATIC_DIR = 'static'
+	static DEFAULT_ID = 'stremio_porn'
+
+	static ID = process.env.STREMIO_PORN_ID || DEFAULT_ID
+	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn.ga'
+	static IS_PROD = process.env.NODE_ENV === 'production'
+	static PORT = process.env.STREMIO_PORN_PORT || process.env.PORT || '80'
+	static EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL
+	static PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY
+	static CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1'
+
+	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn.ga'
+	static PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY
+	static CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1'
+	static EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL
+
+}
+
+export default Config
