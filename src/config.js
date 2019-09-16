1e19528d9df48d0361699b50121ab708cc299725
BoredLama <43473329+BoredLama@users.noreply.github.com>
2019-09-16 15:14:27 +0300

    Update endpoint

--- src/config.js
+++ src/config.js
@@ -7,14 +7,14 @@ class Config {
 	static STATIC_DIR = 'static'
 
 	static ID = process.env.STREMIO_PORN_ID || DEFAULT_ID
-	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn.ga'
+	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn-plus.now.sh'
 	static IS_PROD = process.env.NODE_ENV === 'production'
 	static PORT = process.env.STREMIO_PORN_PORT || process.env.PORT || '80'
 	static EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL
 	static PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY
 	static CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1'
 
-	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn.ga'
+	static ENDPOINT = process.env.STREMIO_PORN_ENDPOINT || 'https://stremio-porn-plus.now.sh'
 	static PROXY = process.env.STREMIO_PORN_PROXY || process.env.HTTPS_PROXY
 	static CACHE = process.env.STREMIO_PORN_CACHE || process.env.REDIS_URL || '1'
 	static EMAIL = process.env.STREMIO_PORN_EMAIL || process.env.EMAIL
